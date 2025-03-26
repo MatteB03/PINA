a4a7b2675d9bf895af42ac4038f7b2c8f7f826b0
MatteB03 <matteo.bertocchi144@gmail.com>
2025-03-26 15:25:54 +0100

    first implementation

--- tutorials/tutorial15/tsproblem.py
+++ tutorials/tutorial15/tsproblem.py
@@ -23,93 +23,52 @@ from pytorch_lightning import seed_everything
 from pina.optim import TorchOptimizer
 
 class TimeSpaceProblem(TimeDependentProblem, SpatialProblem):
-    output_variables = ["V", "W"]
-    spatial_domain = CartesianDomain({"x": [0.1, 20]})
-    Tf = 70
-    temporal_domain = CartesianDomain({"t": [1, Tf]})
+    output_variables = ["u"]
+    spatial_domain = CartesianDomain({"x": [0, 1]})
+    temporal_domain = CartesianDomain({"t": [0,1]})
 
     # defining the ode equation
-    def aliev_panfilov_equation1(input_, output_):
+    def equation_4p5(input_, output_):
         # computing the derivative
-        V_t = grad(output_, input_, components=["V"], d=["t"])
-        nabla_V = laplacian(output_, input_, components=["V"], d=["x"])
+        u_t = grad(output_, input_, components=["u"], d=["t"])
+        nabla_u = laplacian(output_, input_, components=["u"], d=["x"])
+        
+        u = output_.extract(["u"])
+        gamma = 1
 
-        # extracting the u input variable
-        V = output_.extract(["V"])
-        W = output_.extract(["W"])
-        # print(V.shape)
-        # data
-        k = 8
-        a = 0.01
-        #a=0.002 #additional
-        D = 0.1  # 7.692e-5#0.1
-        #D=0.02 #additional
-        t = input_.extract(["t"])
-        x = input_.extract(["x"])
-
-        res_V = (
-            (-V_t) + D * nabla_V - k * V * (V - a) * (V - 1) - V * W
-        )  # + I_stim #+ 0.1
-
-        return res_V
-
-    def aliev_panfilov_equation2(input_, output_):
-        # computing the derivative
-        W_t = grad(output_, input_, components=["W"], d=["t"])
-
-        # extracting the u input variable
-        V = output_.extract(["V"])
-        W = output_.extract(["W"])
-
-        # data
-        k = 8
-        eps = 0.002
-        mu1 = 0.2  # 0.2
-        mu2 = 0.3
-        b = 0.15  # 0.15
-        #b=0.075 #additional
-        res_W = W_t - ((eps + mu1 * W / (V + mu2)) * (-W - k * V * (V - b - 1)))
-        return res_W
+        res = u_t - gamma * nabla_u + u**3 - u
+        return res
+        
 
     def initial_cond_V(input_, output_):
         x = input_.extract(["x"])
         #return output_.extract(["V"]) - torch.exp(-0.8 * x)  # V_0 #original
-        return output_.extract(["V"])-torch.exp(-0.05*x)
+        return output_.extract(["V"])-torch.exp(-0.8 * x)
     def initial_cond_W(input_, output_):
         x = input_.extract(["x"])
-        return output_.extract(["W"]) - 2.5 * torch.exp(-0.08 * x)  # W_0
+        return output_.extract(["W"]) - 2.5 * torch.exp(-0.8 * x)  # W_0
         #return output_.extract(["W"]) - 2* torch.exp(-0.8 * x)  # W_0 #original
     
 
     # conditions to hold
     # specify the fixed gradient direction (ex x,y...)
     conditions = {
-        "gamma1": Condition(
-            domain=CartesianDomain({"x": 0.1, "t": [1, Tf]}),
-            equation=FixedGradient(0.0, components=["V"], d=["x"]),
-        ),
-        "gamma2": Condition(
-            domain=CartesianDomain({"x": 20, "t": [1, Tf]}),
-            equation=FixedGradient(0.0, components=["V"], d=["x"]),
-        ),
-    #conditions={
-        "t0_V": Condition(
-            domain=CartesianDomain({"x": [0.1, 20], "t": 1}),
-            equation=Equation(initial_cond_V),
-        ),  # FixedValue(0.01, components=['V']),data_weight=1),
-        "t0_W": Condition(
-            domain=CartesianDomain({"x": [0.1, 20], "t": 1}),
-            equation=Equation(initial_cond_W),
-        ),  # FixedValue(0.01, components=['W']),data_weight=1),
-        "D_V": Condition(
-            domain=CartesianDomain({"x": [0.1, 20], "t": [1, Tf]}),
-            equation=Equation(aliev_panfilov_equation1),
-        ),
-        "D_W": Condition(
-            domain=CartesianDomain({"x": [0.1, 20], "t": [1, Tf]}),
-            equation=Equation(aliev_panfilov_equation2),
-        ),
-        #"dataV": Condition(input=input_domain, target=V_0),
-        #"dataW": Condition(input=input_domain, target=W_0)
-        #'data1': Condition(input_points=TX, output_points=Vsav_Wsav_labeled)#,data_weight=1),
-    }
+        "step1": Condition(
+            domain=CartesianDomain({"x": [0,0.249], "t": 0}),
+            equation=FixedValue(0)),
+        "step2": Condition(
+            domain=CartesianDomain({"x": [0.25,0.75], "t": 0}),
+            equation=FixedValue(1)),
+        "step3": Condition(
+            domain=CartesianDomain({"x": [0.751,1], "t": 0}),
+            equation=FixedValue(0)),
+        "L_u": Condition(
+            domain=CartesianDomain({"x":[0,1],"t":[0,1]}),
+            equation=Equation(equation_4p5)),
+        "u0":Condition(
+            domain=CartesianDomain({"x":0,"t":[0,1]}),
+            equation= FixedValue(0)),
+        "u1":Condition(
+            domain=CartesianDomain({"x":1,"t":[0,1]}),
+            equation= FixedValue(0))
+        }
--- tutorials/tutorial15/tutorial.py
+++ tutorials/tutorial15/tutorial.py
@@ -19,33 +19,16 @@ from pina.model import FeedForward
 from pina.callback import MetricTracker, PINAProgressBar, SwitchOptimizer
 from pina.loss import LpLoss
 
-from tutorials.tutorial15.tuner import TimeSpaceProblem
+from tsproblem import TimeSpaceProblem
 from pytorch_lightning import seed_everything
 from pina.optim import TorchOptimizer
 
-snap=1
-
-domain_ = np.linspace(0.1, 20, 200)
-domain_lol = torch.tensor([[value] for value in domain_], dtype=torch.float32)  
-time = torch.ones(domain_lol.shape[0], 1, dtype=torch.float32)
-time=time*snap
-domain_with_time = torch.cat((domain_lol, time), dim=1) 
-labels = ['x', 't']
-input_domain = LabelTensor(domain_with_time, labels=labels)
-
-data = io.loadmat("../../../EP-PINNs/data_files/Aliev_Panfilov_Model/_1D/data_1d_left.mat")
-Vin = torch.tensor(data['Vsav'][snap-1,:],dtype=torch.float32).unsqueeze(1)
-Win = torch.tensor(data['Wsav'][snap-1,:],dtype=torch.float32).unsqueeze(1)
-VinT=torch.cat((Vin, time), dim=1)
-WinT=torch.cat((Win, time), dim=1)
-V_0 = LabelTensor(VinT, labels=['V','t'])
-W_0 = LabelTensor(WinT, labels=['W','t'])
-
 problem = TimeSpaceProblem()
 
-problem.discretise_domain(4000, "random", domains=["D_V", "D_W"])
-problem.discretise_domain(140, "grid", domains=["t0_V", "t0_W"])
-problem.discretise_domain(200, "grid", domains=["gamma2", "gamma1"])
+problem.discretise_domain(4000, "random", domains=["L_u"])
+problem.discretise_domain(250, "grid", domains=["step1", "step3"])
+problem.discretise_domain(251, "grid", domains=["step2"])
+problem.discretise_domain(101, "grid", domains=["u0","u1"])
 
 #print("Input points:", problem.input_pts)
 
@@ -59,97 +42,25 @@ problem.discretise_domain(200, "grid", domains=["gamma2", "gamma1"])
 seed_everything(42, workers=True)
 
 # build the model
-'''model = FeedForward(
-    layers=[32, 32, 32, 32],
+model = FeedForward(
+    layers=[16, 16, 16, 16],
     func=torch.nn.Tanh,  # Tanh,
     output_dimensions=len(problem.output_variables),
     input_dimensions=len(problem.input_variables),
 )
-
-class MyModel2(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.space = FeedForward(
-            layers=[32, 32, 32], func=torch.nn.Softplus, input_dimensions=1, output_dimensions=50)
-
-        #self.coeff_time = torch.nn.Parameter(torch.rand(1))
-        self.time = FeedForward(
-            layers=[32, 32, 32], func=torch.nn.Softplus, input_dimensions=1, output_dimensions=50)
-        
-        self.reduction = torch.nn.Linear(50, 2)
-
-    def forward(self, x):
-        t = self.time(x.extract(["t"]))
-        x = self.space(x.extract(["x"]))
-        return self.reduction(t * x)
-        # return tmp'''
-
-
-
-class MyModel(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.space_w = FeedForward(
-            layers=[32]*3, func=torch.nn.Tanh, input_dimensions=1, output_dimensions=1)
-        self.space_v = FeedForward(
-            layers=[32]*3, func=torch.nn.Tanh, input_dimensions=1, output_dimensions=1)
-        self.coeff_time = torch.nn.Parameter(torch.Tensor([1]))
-        self.known_term = torch.nn.Parameter(torch.Tensor([10]))
-
-    def forward(self, x):
-        x_ = x.extract(["x"])
-        v_shift = 0 
-        w_shift = 20
-        v = self.space_v(x_ + self.known_term - self.coeff_time * (x.extract(["t"]) - v_shift))
-        w = self.space_w(x_ + self.known_term - self.coeff_time * (x.extract(["t"]) - w_shift))
-
-
-
-
-        v.labels = ["V"]
-        w.labels=["W"]
-        return torch.cat([v, w], dim=1)
-        #return self.space(x_ - self.coeff_time*x.extract(["t"]))
-
-
-
-        v = self.space_v(x_ - self.coeff_time*(x.extract(["t"])))
-        w = self.space_w(x_  - self.coeff_time*(x.extract(["t"])))
-
-
-
-
-model = MyModel()
-problem_initial = problem
-#problem_initial = TimeSpaceProblem()
-#del problem_initial.conditions["D_V"]
-#del problem_initial.conditions["D_W"]
-#del problem_initial.conditions["gamma1"]
-#del problem_initial.conditions["gamma2"]
-#del problem_initial.conditions["t0_V"]
-#del problem_initial.conditions["t0_W"]
-#problem_initial.discretise_domain(1400, "grid", domains=["t0_V", "t0_W"])
-
-#print(problem_initial.conditions)
-#print(problem_initial.domains)
-#problem_initial.discretise_domain(70,"grid", domains=["D_V","D_W","t0_V","t0_W","gamma1","gamma2"])
 pinn = PINN(
-    problem_initial,
+    problem,
     model,
     optimizer=TorchOptimizer(torch.optim.Adam, lr=1e-4, weight_decay=0),
     loss=torch.nn.MSELoss()
 )  
-pinn2 = SAPINN(problem_initial,
-    model,
-    optimizer_model=TorchOptimizer(torch.optim.AdamW, lr=1e-3, weight_decay=0),
-    loss=torch.nn.MSELoss()
-)  
-#from lightning.pytorch.loggers import TensorBoardLogger
 
+from lightning.pytorch.loggers import TensorBoardLogger
 trainer = Trainer(
     solver=pinn,
-    max_epochs=5000,#logger=TensorBoardLogger('tutorial_logs'),
+    max_epochs=10000,#logger=TensorBoardLogger('tutorial_logs'),
     accelerator="cpu",
+    logger=TensorBoardLogger(save_dir="training_logs"),
     enable_model_summary=False,
     gradient_clip_val=0.7,
     callbacks=[MetricTracker()],
@@ -162,70 +73,15 @@ trainer = Trainer(
 trainer.train()
 trainer.test()
 
-input_test = CartesianDomain({"x": [0.1, 20], "t": [1, 70]}).sample(70, mode='grid')
+input_test = CartesianDomain({"x": [0, 1], "t": [0,1]}).sample(101, mode='grid')
 #input_test = CartesianDomain({"x": [0.1, 20], "t": [1, 70]}).sample(100, mode='grid')
 output_test = pinn(input_test)
-V = output_test.extract(["V"])
-W = output_test.extract(["W"])
-
-from equation import AlievPanfilov1D_RK_Istim
-import matplotlib.pyplot as plt
-
-Vsav, Wsav = AlievPanfilov1D_RK_Istim()
-
-
-#print(W.detach().numpy().shape)
-NewW=Wsav.flatten()
-#print(len(NewW))
-
+u = output_test.extract(["u"])
 
 plt.figure(figsize=(12, 10))
-
-# V plot
-plt.subplot(3, 2, 1) 
-plt.scatter(input_test.extract(["x"]), input_test.extract(["t"]), c=V.detach().numpy(), cmap='coolwarm')
+plt.scatter(input_test.extract(["x"]), input_test.extract(["t"]), c=u.detach().numpy(), cmap='coolwarm')
 plt.colorbar()
-plt.title('V')
-
-# W plot
-plt.subplot(3, 2, 2)
-plt.scatter(input_test.extract(["x"]), input_test.extract(["t"]), c=W.detach().numpy(), cmap='coolwarm')
-plt.colorbar()
-plt.title('W')
-
-# Vsav plot
-plt.subplot(3, 2, 3)
-plt.title('V (AU)')
-plt.scatter(input_test.extract(["x"]), input_test.extract(["t"]), c=Vsav.flatten(), cmap='coolwarm')
-plt.ylabel('Time (AU)')
-plt.xlabel('x (cells)')
-plt.colorbar()
-
-# Wsav plot
-plt.subplot(3, 2, 4)
-plt.title('W (AU)')
-plt.scatter(input_test.extract(["x"]), input_test.extract(["t"]), c=Wsav.flatten(), cmap='coolwarm')
-plt.ylabel('Time (AU)')
-plt.xlabel('x (cells)')
-plt.colorbar()
-
-# V - Vsav difference
-plt.subplot(3, 2, 5)
-plt.title('V - Vsav')
-V_diff = V.detach().numpy().flatten() - Vsav.flatten()
-plt.scatter(input_test.extract(["x"]), input_test.extract(["t"]), c=V_diff, cmap='coolwarm')
-plt.ylabel('Time (AU)')
-plt.xlabel('x (cells)')
-plt.colorbar()
-
-# W - Wsav difference
-plt.subplot(3, 2, 6)
-plt.title('W - Wsav')
-W_diff = W.detach().numpy().flatten() - Wsav.flatten()
-plt.scatter(input_test.extract(["x"]), input_test.extract(["t"]), c=W_diff, cmap='coolwarm')
-plt.ylabel('Time (AU)')
-plt.xlabel('x (cells)')
-plt.colorbar()
-
+plt.title('u')
+plt.grid()
 plt.tight_layout() 
 plt.show()
